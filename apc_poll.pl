92de7fd2750cfeb03357fbd372d2ce6a4cb58c53
Brad Davis <brd@FreeBSD.org>
2014-07-01 15:08:31 -0600

    - Add mode testing - Flush out the beginnings of Nagios and Graphite mode - Move SNMP polling into a separate function so it can be called multiple times - Update the config example to show the new settings

--- apc_poll.pl
+++ apc_poll.pl
@@ -10,11 +10,39 @@ use strict;
 use warnings;
 use Net::SNMP;
 use IO::Socket;
+no warnings 'uninitialized';
 
 # Check which mode we are running in
 # Nagios or Graphite
 my $args = $#ARGV + 1;
 if($args == 0) {
+	usage();
+}
+elsif($args == 1) {
+	if($ARGV[0] eq "graphite") {
+		graphite();
+	}
+	else {
+		usage();
+	}
+}
+elsif($args == 4 || $args == 5) {
+	if($ARGV[0] eq "nagios") {
+		if ($args == 4) {
+			nagios(4, $ARGV[1], $ARGV[2], $ARGV[3]);
+		}
+		elsif($args == 5) {
+			nagios(5, $ARGV[1], $ARGV[2], $ARGV[3], $ARGV[4]);
+		}
+		else {
+			usage();
+		}
+	}
+	else {
+		usage();
+	}
+}
+else {
 	usage();
 }
 
@@ -24,63 +52,112 @@ our (%hostlist, $debug, $graphite_enable, $graphite_host, $graphite_path);
 # Include the config
 require 'config.pl' or die "Unable to open config.pl: $!";
 
-# OIDs for the PDUs
-my $phase_power_oid  = ".1.3.6.1.4.1.318.1.1.12.1.16.0";
-my $phase_current_oid = ".1.3.6.1.4.1.318.1.1.12.2.3.1.1.2.1";
-my $bank1_oid = ".1.3.6.1.4.1.318.1.1.12.2.3.1.1.2.2";
-my $bank2_oid = ".1.3.6.1.4.1.318.1.1.12.2.3.1.1.2.3";
-
 # The Current Epoch time
 my $epoch = time;
 
-while ( my ($host, $community) = each %hostlist) {
 
-	my ($session, $error) = Net::SNMP->session(
-		Hostname => "$host",
-		Community => $community,
-	) or warn "Session: $!";
-	
-	# Poll
-	my $phase_power = $session->get_request("$phase_power_oid")     or warn "failed to poll phase power on $host: $!";
-	my $phase_current = $session->get_request("$phase_current_oid") or warn "failed to poll phase current on $host: $!";
-	my $bank1_current = $session->get_request("$bank1_oid")	 or warn "failed to poll bank1 current on $host: $!";
-	my $bank2_current = $session->get_request("$bank2_oid")	 or warn "failed to poll bank2 current on $host: $!";
+sub snmppoll {
 
-	print "hostname: $host\n";
-	print "phase power is:   $phase_power->{$phase_power_oid}\n";
-	print "phase current is: " . $phase_current->{$phase_current_oid} / 10 . "\n";
-	print "bank1 current is: " . $bank1_current->{$bank1_oid} / 10 . "\n";
-	print "bank2 current is: " . $bank2_current->{$bank2_oid} / 10 . "\n";
-	
-	# If Graphite
-	if ( $graphite_enable == 1 && $graphite_host ) {
-		# Open connection
-		print "sending $host data to graphite..";
-		my $client = IO::Socket::INET->new(
-				Proto => 'tcp',
-				PeerAddr => $graphite_host,
-				PeerPort => 2003,
-			) or die "Cannot connect: $!";
-	
-		# Write the data to graphite
-		print $client "$graphite_path.$host.watts "   . $phase_power->{$phase_power_oid}          . " " . $epoch . "\n";
-		print $client "$graphite_path.$host.current " . $phase_current->{$phase_current_oid} / 10 . " " . $epoch . "\n";
-		print $client "$graphite_path.$host.bank1 "   . $bank1_current->{$bank1_oid} / 10         . " " . $epoch . "\n";
-		print $client "$graphite_path.$host.bank2 "   . $bank2_current->{$bank2_oid} / 10         . " " . $epoch . "\n";
-		close $client;
+	 my ($host, $community) = @_;
+	 my %snmpresults;
+
+	 # OIDs for the PDUs
+	 my $num_banks_oid = ".1.3.6.1.4.1.318.1.1.12.2.1.4.0";
+	 my $phase_power_oid  = ".1.3.6.1.4.1.318.1.1.12.1.16.0";
+	 my $phase_current_oid = ".1.3.6.1.4.1.318.1.1.12.2.3.1.1.2.1";
+	 my $bank1_oid = ".1.3.6.1.4.1.318.1.1.12.2.3.1.1.2.2";
+	 my $bank2_oid = ".1.3.6.1.4.1.318.1.1.12.2.3.1.1.2.3";
+
+	 say "snmppoll(): host: $host, community: \"$community\"";
+
+	 my ($session, $error) = Net::SNMP->session(
+			Hostname => $host,
+			Community => $community,
+		) or warn "Unable to create Session: $!";
+
+	 # Poll
+	 my $num_banks = $session->get_request($num_banks_oid) or warn "failed to poll number of banks on $host: $!";
+	 say "banks: $num_banks->{$num_banks_oid}";
+	 if($num_banks->{$num_banks_oid} eq 0) {
+	 	# AP7921
+		say "banks = 0; ap7921";
+	 	my $phase_power = $session->get_request("$phase_power_oid")     or warn "failed to poll phase power on $host: $!";
+	 	my $phase_current = $session->get_request("$phase_current_oid") or warn "failed to poll phase current on $host: $!";
+		$snmpresults{$host}{$phasepower} = $phase_power->{$phase_power_oid};
+		$snmpresults{$host}{$phasecurrent} = $phase_current->{$phase_current_oid} / 10;
+	 	say "phase power is: " . $phase_power->{$phase_power_oid};
+	 	say "phase current is: " . $phase_current->{$phase_current_oid} / 10;
+	 }
+	 elsif($num_banks->{$num_banks_oid} eq 2) {
+	 	# AP8941
+		say "banks = 2; ap8941";
+	 	my $phase_power = $session->get_request("$phase_power_oid")     or warn "failed to poll phase power on $host: $!";
+	 	my $phase_current = $session->get_request("$phase_current_oid") or warn "failed to poll phase current on $host: $!";
+	 	my $bank1_current = $session->get_request("$bank1_oid")	 or warn "failed to poll bank1 current on $host: $!";
+	 	my $bank2_current = $session->get_request("$bank2_oid")	 or warn "failed to poll bank2 current on $host: $!";
+	 	say "phase power is: " . $phase_power->{$phase_power_oid};
+	 	say "phase current is: " . $phase_current->{$phase_current_oid} / 10;
+	 	say "bank1 current is: " . $bank1_current->{$bank1_oid} / 10;
+	 	say "bank2 current is: " . $bank2_current->{$bank2_oid} / 10;
+	 }
+	 else {
+	 	say "banks unknown";
+		$snmpresults{error} = 1;
+		$snmpresults{errorstr} = "Unable to poll $host";
+	 }
+
+	 return %snmpresults;
+}
+
+
+sub nagios {
+
+	 my ($numofargs, $host, $community, $warn, $crit) = @_;
+	 my $session = snmppoll($host, $community);
+
+	 # check rPDULoadStatusLoadState and warn if phaseLoadNearOverload(3) and crit if phaseLoadOverload(4)
+	 #if ($phase_current > 30) {
+	 	# XXX: Add missing check to see if phase current is greater than 60%
+	 #}
+	 # Print the nicely formatted Nagios line
+	 say "";
+	 exit
+}
+
+
+sub graphite {
+	while ( my ($host, $community) = each %hostlist) {
+
+		my $session = snmppoll($host, $community);
+
+		# If Graphite
+		if ( $graphite_enable == 1 && $graphite_host ) {
+			# Open connection
+			print "sending $host data to graphite..";
+			my $client = IO::Socket::INET->new(
+					Proto => 'tcp',
+					PeerAddr => $graphite_host,
+					PeerPort => 2003,
+				) or die "Cannot connect: $!";
+
+			# Write the data to graphite
+			#print $client "$graphite_path.$host.watts "   . $phase_power->{$phase_power_oid}          . " " . $epoch . "\n";
+			#print $client "$graphite_path.$host.current " . $phase_current->{$phase_current_oid} / 10 . " " . $epoch . "\n";
+			#print $client "$graphite_path.$host.bank1 "   . $bank1_current->{$bank1_oid} / 10         . " " . $epoch . "\n";
+			#print $client "$graphite_path.$host.bank2 "   . $bank2_current->{$bank2_oid} / 10         . " " . $epoch . "\n";
+			close $client;
+		}
+
+		# If debugging is enabled
+		if ( $debug == 1 && $debug ) {
+			#print "$graphite_path.$host.watts "   . $phase_power->{$phase_power_oid}          . " " . $epoch . "\n";
+			#print "$graphite_path.$host.current " . $phase_current->{$phase_current_oid} / 10 . " " . $epoch . "\n";
+			#print "$graphite_path.$host.bank1 "   . $bank1_current->{$bank1_oid} / 10         . " " . $epoch . "\n";
+			#print "$graphite_path.$host.bank2 "   . $bank2_current->{$bank2_oid} / 10         . " " . $epoch . "\n";
+		}
+
+		$session->close();
 	}
-
-	# If debugging is enabled
-	if ( $debug == 1 && $debug ) {
-		print "$graphite_path.$host.watts "   . $phase_power->{$phase_power_oid}          . " " . $epoch . "\n";
-		print "$graphite_path.$host.current " . $phase_current->{$phase_current_oid} / 10 . " " . $epoch . "\n";
-		print "$graphite_path.$host.bank1 "   . $bank1_current->{$bank1_oid} / 10         . " " . $epoch . "\n";
-		print "$graphite_path.$host.bank2 "   . $bank2_current->{$bank2_oid} / 10         . " " . $epoch . "\n";
-	}
-	
-	print " done\n";
-	
-	$session->close();
 }
 
 
